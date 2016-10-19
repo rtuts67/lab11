58edf0da82b2d6d14dfd5911d70300e9e5ac0048
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-10-18 20:13:39 -0700

    Refactoring render and random images to be in one object, got random index to work

--- app.js
+++ app.js
@@ -1,66 +1,79 @@
 'use strict';
-var allPictures = [];
-var randomArray = [];
-var Randompicture = function(name, src) {
+
+var Randompicture = function(name, path) {
   this.name = name;
-  this.src = src;
-  allPictures.push(this);
+  this.path = path;
+  this.votes = 0;
+  trackvotes.allPictures.push(this);
 };
 
-new Randompicture('r2D2', 'images/bag.jpg');
-new Randompicture('banana', 'images/banana.jpg');
-new Randompicture('bathroom', 'images/bathroom.jpg');
-new Randompicture('boots', 'images/boots.jpg');
-new Randompicture('breakfeast', 'images/breakfast.jpg');
-new Randompicture('bubblegum', 'images/bubblegum.jpg');
-new Randompicture('chair', 'images/chair.jpg');
-new Randompicture('cthulhu', 'images/cthulhu.jpg');
-new Randompicture('dogduck', 'images/dog-duck.jpg');
-new Randompicture('dragon', 'images/dragon.jpg');
-new Randompicture('pen', 'images/pen.jpg');
-new Randompicture('petsweep', 'images/pet-sweep.jpg');
-new Randompicture('scissors', 'images/scissors.jpg');
-new Randompicture('shark', 'images/shark.jpg');
-new Randompicture('sweep', 'images/sweep.png');
-new Randompicture('tauntaun', 'images/tauntaun.jpg');
-new Randompicture('unicorn', 'images/unicorn.jpg');
-new Randompicture('usb', 'images/usb.gif');
-new Randompicture('water', 'images/water-can.jpg');
-new Randompicture('wineglass', 'images/wine-glass.jpg');
+var trackvotes = {
+  allPictures: [],
 
-function getRandomImage(path) {
-  var number = Math.floor( Math.random() * allPictures.length );
-  path = allPictures[number].src;
-  var imgStr = '<img src="' + path + '" alt = "">';;
-  document.write(imgStr); document.close();
-};
-getRandomImage(allPictures);
+  createNewPictures: function() {
+    new Randompicture('r2D2', 'images/bag.jpg');
+    new Randompicture('banana', 'images/banana.jpg');
+    new Randompicture('bathroom', 'images/bathroom.jpg');
+    new Randompicture('boots', 'images/boots.jpg');
+    new Randompicture('breakfeast', 'images/breakfast.jpg');
+    new Randompicture('bubblegum', 'images/bubblegum.jpg');
+    new Randompicture('chair', 'images/chair.jpg');
+    new Randompicture('cthulhu', 'images/cthulhu.jpg');
+    new Randompicture('dogduck', 'images/dog-duck.jpg');
+    new Randompicture('dragon', 'images/dragon.jpg');
+    new Randompicture('pen', 'images/pen.jpg');
+    new Randompicture('petsweep', 'images/pet-sweep.jpg');
+    new Randompicture('scissors', 'images/scissors.jpg');
+    new Randompicture('shark', 'images/shark.jpg');
+    new Randompicture('sweep', 'images/sweep.png');
+    new Randompicture('tauntaun', 'images/tauntaun.jpg');
+    new Randompicture('unicorn', 'images/unicorn.jpg');
+    new Randompicture('usb', 'images/usb.gif');
+    new Randompicture('water', 'images/water-can.jpg');
+    new Randompicture('wineglass', 'images/wine-glass.jpg');
+  },
 
-function randomPictures() {
-  for (var i = 0; i < 2; i++) {
-    randomArray.push(getRandomImage(allPictures));
+  getRandomNumber: function() {
+    return Math.floor(Math.random() * this.allPictures.length);
+  },
+  createIndexes: function(array) {
+    var index1 = this.getRandomNumber(array);
+    var index2 = this.getRandomNumber(array);
+    while (index2 === index1) {
+      index2 = this.getRandomNumber(array);
+    };
+
+    var index3 = this.getRandomNumber(array);
+    while (index3 === index2 || index3 === index1) {
+      index3 = this.getRandomNumber(array);
+    };
+    return [index1, index2, index3];
   }
 };
+  //randomPictures: function () {
+    //for (var i = 0; i < 2; i++) {
+      //this.randomArray.push(this.getRandomNumber(this.allPictures[i]));
+    //}
+  //},
 
-randomPictures();
+  //renderPics: function () {
+    //var parentRenderEl = document.getElementById('img1');
+    //trackvotes.img1.src = trackvotes.path;
+    //var childRender =
+    //parentRenderEl.appendChild(childRender);
 
-function renderFirstPic () {
-  var parentRenderEl = document.getElementById('img1');
-  var childRender = randomArray[0];
-  parentRenderEl.appendChild(childRender);
-}
-renderFirstPic();
+    //var parentRenderEl2 = document.getElementById('img2');
+    //this.img2.src = trackvotes.randomArray[1].path;
+  //  var childRender2 = trackvotes.randomArray[1];
+  //  parentRenderEl2.appendChild(childRender2);
 
-function renderSecondPic () {
-  var parentRenderEl2 = document.getElementById('img2');
-  var childRender2 = randomArray[1];
-  parentRenderEl2.appendChild(childRender2);
-}
-renderSecondPic();
+  //  var parentRenderEl3 = document.getElementById('img3');
+  //  trackvotes.img3.src = trackvotes.randomArray[3].path;
+  //  var childRender3 = trackvotes.randomArray[3];
+  //  parentRenderEl3.appendChild(childRender3);
+//  },
+//};
 
-function renderThirdPic () {
-  var parentRenderEl3 = document.getElementById('img3');
-  var childRender3 = randomArray[2];
-  parentRenderEl3.appendChild(childRender3);
-}
-renderThirdPic();
+trackvotes.createNewPictures();
+//trackvotes.randomPictures();
+//trackvotes.renderPics();
