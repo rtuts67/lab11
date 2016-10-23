77466227f31c534324a746f5779504e5fe884469
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-10-22 17:04:36 -0700

    changed colors on chart

--- app.js
+++ app.js
@@ -103,6 +103,8 @@ var trackvotes = {
 
   makeChart: function() {
     trackvotes.updateMyChart();
+    var display = document.getElementById('chartplace');
+    display.style.visibility = 'visible';
     var ctx = document.getElementById('myChart');
     var myChart = new Chart(ctx, {
       type: 'bar',
@@ -111,22 +113,8 @@ var trackvotes = {
         datasets: [{
           label: '# of Votes',
           data: trackvotes.chartVotes,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
+          backgroundColor: '#008080',
+          borderColor: '#48D1CF',
           borderWidth: 1
         }]
       },
